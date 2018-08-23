2356d706d66d04c51dcf5145421a73abdcb7cade
Ancher Tunchar <anchertunchar@gmail.com>
2018-08-23 13:10:00 +0300

    cached the dom

---
+++ engine.js
@@ -0,0 +1,25 @@
+var topDisplay = document.getElementById("topDisplay");
+var botDisplay = document.getElementById("botDisplay");
+
+const buttonC = document.getElementById("buttonC");
+const buttonPlusMinus = document.getElementById("buttonPlusMinus");
+const buttonPerc = document.getElementById("buttonPerc");
+const buttonSqrt = document.getElementById("buttonSqrt");
+const button7 = document.getElementById("button7");
+const button8 = document.getElementById("button8");
+const button9 = document.getElementById("button9");
+const buttonDevi = document.getElementById("buttonDevi");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+const button6 = document.getElementById("button6");
+const buttonX = document.getElementById("buttonX");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const buttonMin = document.getElementById("buttonMin");
+const button0 = document.getElementById("button0");
+const button00 = document.getElementById("button00");
+const buttonDot = document.getElementById("buttonDot");
+const buttonPlus = document.getElementById("buttonPlus");
+
+
