442c4a0cad6703574de2677366b13d3934e2cf17
Ancher Tunchar <anchertunchar@gmail.com>
2018-08-29 19:16:42 +0300

    fixed decimal bug, fixed negative number bug, can now string multiple calculations

--- engine.js
+++ engine.js
@@ -8,22 +8,54 @@ var z; //result variable for function eq
 
 function insert(num)
 {
+    
+    if(topDisplay.innerHTML.length > 12)
+    {
+        alert("I'm not that smart!");
+    }
+    else
+    {
     topDisplay.innerHTML = topDisplay.innerHTML+num;
     x = topDisplay.innerHTML;
     x = parseFloat(x);
+    }
+}
+function insert00(num)
+{
+    if(topDisplay.innerHTML.length > 12)
+    {
+        alert("I'm not that smart!");
+    }
+    else
+    {
+    topDisplay.innerHTML = topDisplay.innerHTML+"00";
+    x = topDisplay.innerHTML;
+    x = parseFloat(x);
+    }
 }
 
 
 function dot()
 {
-    topDisplay.innerHTML = topDisplay.innerHTML+".";
-    console.log("dot");
+    
+
+    if (topDisplay.innerHTML.includes("."))
+    {
+        console.log("dot");
+    }
+    else
+    { 
+        topDisplay.innerHTML = topDisplay.innerHTML+".";
+        console.log("dot");
+    }
+
 }
 
 function clean()
 {
     topDisplay.innerHTML = "";
     numbers = [];
+    botDisplay.innerHTML = "";
 }
 
 function eq()
@@ -65,7 +97,15 @@ function operator()
 
 function negative()
 {
-    topDisplay.innerHTML = "-"+topDisplay.innerHTML;
+    if (topDisplay.innerHTML.length < 2)
+    {
+        topDisplay.innerHTML = "-"+topDisplay.innerHTML;
+    }
+
+    if (topDisplay.innerHTML.length == 2)
+    {
+        topDisplay.innerHTML = "";
+    }
 }
 
 function calc(num1)
@@ -75,25 +115,50 @@ function calc(num1)
     {
         case 1:
             z = numbers[0]+numbers[1];
+                if (numbers.length == 2)
+                {
+                    botDisplay.innerHTML = z;
+                    numbers[0] = z;
+                    numbers.length = 1;
+                } 
             console.log("plus");
             break;
         case 2:
             z = numbers[0]-numbers[1];
+                if (numbers.length == 2)
+                {
+                    botDisplay.innerHTML = z;
+                    numbers[0] = z;
+                    numbers.length = 1;
+                } 
             console.log("minus");
             break;
         case 3:
             z = numbers[0]*numbers[1];
+                if (numbers.length == 2)
+                {
+                    botDisplay.innerHTML = z;
+                    numbers[0] = z;
+                    numbers.length = 1;
+                } 
             console.log("multiply");
             break;
         case 4:
 
             if(numbers[1] == "0")
                 {
+                    alert("No no no");
                     location.reload();
                 }
             else 
                 {
                     z = numbers[0]/numbers[1];
+                        if (numbers.length == 2)
+                        {
+                            botDisplay.innerHTML = z;
+                            numbers[0] = z;
+                            numbers.length = 1;
+                        } 
                 }
 
             console.log("devide");
